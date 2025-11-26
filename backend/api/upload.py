f1fcb2f27cbe0453769e7dd10a828da33c32b878
MaoSLv <75819457+MaoSLv@users.noreply.github.com>
2025-11-26 23:17:32 +0800

    Delete backend directory

--- backend/api/upload.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-
-router = APIRouter(prefix="/upload", tags=["上传"])
-
-@router.post("/audio")
-async def upload(file: UploadFile = File(...)):
-    print(file)
-    return "hello 123"
